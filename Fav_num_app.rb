95045f73a71e3f2650aabcc3fb1e7baa811c1ac1
TMerrick55 <tmerrick44@gmail.com>
2017-02-18 15:14:46 -0500

    Sinatra Favorite Num Ver. 2.0 (FINAL)

--- Fav_num_app.rb
+++ Fav_num_app.rb
@@ -1,13 +1,12 @@
 require 'sinatra'
 
 get '/' do
-	
-erb :name_input
+		erb :name_input
 end
 
 post '/name' do
 	name_backend = params[:name_input]
-	erb :age_input, locals => {:username => name_backend}
+	erb :age_input, :locals => {:username => name_backend}
 end
 
 post '/age' do
@@ -16,22 +15,22 @@ post '/age' do
 	erb :fav_nums, :locals => {:username => name_backend, :age => age_backend}
 end
 
-post '/your_favorite_numbers' do
+post '/favorite_numbers' do
 	name_backend = params[:name_input]
 	age_backend = params[:age_input].to_i
-	num1_backend = params[:num1_fav_input].to_i
-	num2_backend = params[:num2_fav_input].to_i
-	num3_backend = params[:num3_fav_input].to_i
+	num1_backend = params[:fav_num1_input].to_i
+	num2_backend = params[:fav_num2_input].to_i
+	num3_backend = params[:fav_num3_input].to_i
 	sum = (num1_backend + num2_backend + num3_backend)
 
 	results = 'less than'
 		if (sum > age_backend)
 			results = 'greater than'
-		elsif (sum ==age_backend)
+		elsif (sum == age_backend)
 			results = 'equal to'
 		else
 			results
 		end
 
-	erb :total, :locals => {username => name_backend, :age => age_backend, :fav1 => num1_backend, :fav2 => num2_backend, :fav3 => num3_backend, :sum => sum, :results => results}
+	erb :total, :locals => {:username => name_backend, :age => age_backend, :fav1 => num1_backend, :fav2 => num2_backend, :fav3 => num3_backend, :sum => sum, :results => results}
 end
